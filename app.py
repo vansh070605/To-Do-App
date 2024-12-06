f30f711350c22248bce97697ca2ba56ddb3ac0bb
Vansh Agrawal <vansh070605@gmail.com>
2024-12-06 12:14:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc =  db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    search_query = request.args.get('search')  # Get the search term from the query parameters
+
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    if search_query:
+        # If there's a search query, filter todos by title or description
+        allTodo = Todo.query.filter(
+            (Todo.title.ilike(f'%{search_query}%')) | 
+            (Todo.desc.ilike(f'%{search_query}%'))
+        ).all()
+    else:
+        # Otherwise, show all todos
+        allTodo = Todo.query.all()
+
+    return render_template('index.html', allTodo=allTodo)
+
+@app.route('/show')
+def products():
+    allTodo = Todo.query.all()
+    print(allTodo)
+    return 'this is products page'
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        db.session.commit()
+        return redirect("/")
+    return render_template('update.html', todo=todo)
+
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+
+@app.route("/delete_all")
+def delete_all():
+    # Delete all records from the Todo table
+    Todo.query.delete()
+    db.session.commit()
+    return redirect("/")  # Redirect back to the homepage 
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
